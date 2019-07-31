520e5b83f0c4b32d5e4990cdb24132c00831c564
Gabe Mansur <gabe_mansur@harvard.edu>
2019-07-31 15:01:40 -0400

    Updated survey link

--- app/Tasks/Conclusion.php
+++ app/Tasks/Conclusion.php
@@ -81,7 +81,7 @@ class Conclusion {
   private $feedbackLinks = [
     'pilot' => 'https://docs.google.com/forms/d/1ZZV8P_dyifFn4DRhalN82126POz3R-SgCWXvTo7DMaU/viewform?ts=5d35ccb7&edit_requested=true',
     'group5Pilot' => 'https://docs.google.com/forms/d/1KNfcSCGmdNy4vqKtx2R8paFvSeklTfQyvtWBPaINRwk/viewform?usp=sharing_eip&ts=5d405d8c&edit_requested=true',
-    'groupTwo' => 'https://docs.google.com/forms/d/e/1FAIpQLSd2dia4FinoOoNlh6hqDAn5ExBuEpVKasOj6YLFEHSsn-wi-Q/viewform?usp=sf_link'
+    'groupTwo' => 'https://urldefense.proofpoint.com/v2/url?u=https-3A__docs.google.com_forms_d_e_1FAIpQLSfL8x7CTs9I6vPreeXAdw5-2DURJMj5G17SqKZuMyCNC-5F1EQw1w_viewform-3Fusp-3Dsf-5Flink&d=DwMFaQ&c=WO-RGvefibhHBZq3fL85hQ&r=tAX8kL7P9f6gcapc67VgDbxuAn6xB0Zl7vfMQBORGpQ&m=23Mvsql-PxIiPW6hhQRvCrbd4sGrl4FDLjm7r5XrFVM&s=Xa0buQFFydQM--IKsMwZf0uuKzfcX4YiwTD54YkoiHE&e='
   ];
 
   private static $avaialbleParams = ['hasIndividuals' => ['true', 'false'], 'hasGroup' => ['false'], 'type' => ['mturk', 'hdsl_individual'], 'hasCode' => ['true', 'false'], 'feedback' => ['true', 'false']];
